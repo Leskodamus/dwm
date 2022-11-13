d31acb2a3b10ef1c5e2c499d4c292dd37655445e
Leskodamus <florian@leskovsek.dev>
2022-11-13 21:39:38 +0100

    Change termcmd to urxvt

--- config.h
+++ config.h
@@ -92,9 +92,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", dmenubg, "-p", dmenuprompt, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, NULL };
+static const char *scratchpadcmd[] = { "urxvt", "-title", scratchpadname, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
